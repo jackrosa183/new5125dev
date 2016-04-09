18b9c58de4d1526d683844945b9c89b8dc502d60
jackrosa183 <jackrosa183@gmail.com>
2016-04-09 18:24:40 -0400

    main menu functionality
    main menu works and gi output glitch is fixed

---
+++ 5125 Ridge Road/Assets/menuscript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class menuscript : MonoBehaviour {
+	public Canvas quitMenu;
+	public Button startText;
+	public Button exitText;
+
+	// Use this for initialization
+	void Start () {
+		quitMenu = quitMenu.GetComponent<Canvas> ();
+		startText = startText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+
+	}
+
+	public void Exitpress()
+
+	{
+		quitMenu.enabled = true;
+		startText.enabled = false;
+		exitText.enabled = false;
+	}
+
+	public void NoPress()
+
+	{
+		quitMenu.enabled = false;
+		startText.enabled = true;
+		exitText.enabled = true;
+	}
+
+	public void StartLevel()
+
+	{
+		SceneManager.LoadScene (1);
+	}
+
+	public void ExitGame()
+
+	{
+		Application.Quit ();
+	}
+
+	
+
+
+
+}
