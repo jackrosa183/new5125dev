5e86f72e0b60a860f7c720f220506ce276c07182
jackrosa183 <jackrosa183@gmail.com>
2016-04-09 17:37:44 -0400

    Initial commit

---
+++ 5125 Ridge Road/Assets/NewBehaviourScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	private float endtime = 50.0f;  // 60 seconds
+
+	IEnumerator Start()
+	{
+	     yield return new WaitForSeconds(endtime);
+		SceneManager.LoadScene(1);
+	}
+}
