128d9a29010f723993bfb0f44ea45e0ec48c5ca8
jackrosa183 <jackrosa183@gmail.com>
2016-04-18 22:14:58 -0400

    oops

---
+++ 5125 Ridge Road/Assets/playaudiomovie.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playaudiomovie : MonoBehaviour {
+	public GameObject other;
+	void Example() {
+		other.GetComponent<AudioSource>().Play();
+	}
+}
